52987aa8a1c37797aef5efe3677409b7ce424d4b
Ruslan Pashin <empty-15@mail.ru>
2020-04-23 20:52:20 +0300

    delete websockethomework

--- Semestr#4/05. WebSocketHomework/src/main/java/ru/itis/javalab/websockethomework/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.javalab.websockethomework.repository;
-
-import ru.itis.javalab.websockethomework.model.User;
-
-import java.util.Optional;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    Optional<User> findUserByEmail(String email);
-}
